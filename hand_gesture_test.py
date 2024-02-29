e252625a1b47171a9a8017880a55ee589bf59450
klsharma22 <30960037+klsharma22@users.noreply.github.com>
2024-03-01 01:13:39 +0530

    Empty Push

--- hand_gesture_test.py
+++ hand_gesture_test.py
@@ -1,11 +1,19 @@
-import imageio as iio
-import matplotlib.pyplot as plt
-import time
-camera = iio.get_reader("<video0>")
-meta = camera.get_meta_data()
-delay = 1/meta["fps"]
-for frame_counter in range(15):
-    frame = camera.get_next_data()
-    time.sleep(delay)
-camera.close()
-plt.imshow(frame)
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
