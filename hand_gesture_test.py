09a3d0c345b0c4fd63533baf44e1de4075116f42
klsharma22 <30960037+klsharma22@users.noreply.github.com>
2024-03-01 15:23:41 +0530

    Trying to integrate mediapipe

--- hand_gesture_test.py
+++ hand_gesture_test.py
@@ -1,19 +1,31 @@
+import mediapipe as mp
 import cv2
+import numpy as np
+import uuid
+import os
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
 
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+with mp_hands.Hands(min_detection_confidence= 0.8, min_tracking_confidence= 0.5) as hands:
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
+        # Detection
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Converting BGR to RGB
+        image.flags.writeable = False # Setting flag to not render anythin on the image
+        results = hands.process(image) # Detecting hand in the image
 
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
+        image.flags.writeable = True # Setting the flag to write on the imgae
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        print(results)
+
+        cv2.imshow('Hand Tracking', frame)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
