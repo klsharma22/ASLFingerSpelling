36e73cf4d03275ecd1547221e3dc9c40602fc4ca
klsharma22 <30960037+klsharma22@users.noreply.github.com>
2024-03-01 19:18:10 +0530

    Made a few changes in structure

--- hand_gesture_test.py
+++ hand_gesture_test.py
@@ -1,6 +1,7 @@
 import mediapipe as mp
 import cv2
 import numpy as np
+import time
 
 BaseOptions = mp.tasks.BaseOptions
 HandLandmarker = mp.tasks.vision.HandLandmarker
@@ -9,28 +10,33 @@ HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 # Create a hand landmarker instance with the live stream mode:
-def print_result(result: HandLandmarkerResult, output_image: mp.Image):
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     print('hand landmarker result: {}'.format(result))
 
 options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path='/Users/klsharma22/Desktop/ASLFingerSpelling/hand_landmarker.task'),
     running_mode=VisionRunningMode.LIVE_STREAM,
-    result_callback=print_result)
+    num_hands=1,
+    min_hand_detection_confidence=0.5,
+    min_hand_presence_confidence=0.5,
+    min_tracking_confidence=0.5,
+    result_callback=print_result
+)
 
 cap = cv2.VideoCapture(0)
-with HandLandmarker.create_from_options(options) as landmarker:
-    while cap.isOpened():
-        ret, frame = cap.read()
+landmarker = HandLandmarker.create_from_options(options)
+while cap.isOpened():
+    ret, frame = cap.read()
 
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
 
-        results = landmarker.detect_async(mp_image)
+    results = landmarker.detect_for_video(mp_image, time.time.now())
 
 
-        cv2.imshow('Hand Tracking', frame)
+    cv2.imshow('Hand Tracking', frame)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
